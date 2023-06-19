1ae04fb6d707a783401bcbbd3e2fbef8a605ca5a
讖淤 <3209889807@qq.com>
2023-06-19 15:30:20 +0800

    调整首页

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -8,35 +8,10 @@ export default memo(function About() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React + TypeScript!</p>
-        <p>
-          <Button type="primary" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </Button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <div className="home-box">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>一个 react + Typescript + Vite Template 练习项目</p>
+        </div>
       </header>
     </div>
   );
